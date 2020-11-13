29c9bd749bfa7d65e528a47b0f96575d1f80fc31
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-13 14:02:41 +0900

    Add Joi for validation and fix entity for pushing PrimaryColumn, other columns

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import * as Joi from "joi";
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ConfigModule } from '@nestjs/config';
+import { Restaurant } from './restaurants/entities/restaurants.entity';
+
 
 @Module({
   imports: [
@@ -11,6 +14,14 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),  
+        DB_PORT: Joi.string().required(),  
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      })
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
@@ -23,8 +34,9 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true,
-      logging: true
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: true,
+      entities: [Restaurant]
     }),
   ],
   controllers: [],
--- src/restaurants/entities/restaurants.entity.ts
+++ src/restaurants/entities/restaurants.entity.ts
@@ -1,17 +1,31 @@
 import { Field, ObjectType } from "@nestjs/graphql";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+    @PrimaryGeneratedColumn()
+    @Field(type => Number)
+    id: number;
+
     @Field(is => String)
+    @Column()
     name: String;
 
     @Field(type => Boolean, { nullable: true})
+    @Column()
     isVegan?: boolean;
 
     @Field(type => String)
+    @Column()
     address: String;
 
     @Field(type => String)
+    @Column()
     ownersName: String;
+
+    @Field(type=> String)
+    @Column()
+    categoryName: string;
 }
