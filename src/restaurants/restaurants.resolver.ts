cd60574aa12b3a8e724d724957031ddcf27dcc69
woals4815 <71315300+woals4815@users.noreply.github.com>
2020-11-13 15:14:14 +0900

    Make repositoryOfRestaurant and make service

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,13 +1,14 @@
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurants.entity";
+import { RestaurantService } from "./restaurants.service";
 
 @Resolver(of => Restaurant)
-export class RestaurantResolver {    
+export class RestaurantResolver {
+    constructor(private readonly restaurantService: RestaurantService){}    
     @Query(returns => [Restaurant])
-    restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[]{
-        console.log(veganOnly)
-        return [];
+    restaurants(): Promise<Restaurant[]>{
+        return this.restaurantService.getAll();
     }
     @Mutation(returns => Boolean)
     createRestaurant(
